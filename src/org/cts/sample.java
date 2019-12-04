a6b78312fa4efa27b10429481b523c3331294ee5
Anisha213 <Anisharose213@gmail.com>
2019-12-03 18:59:22 -0800

    Code added to branch2

--- src/org/cts/sample.java
+++ src/org/cts/sample.java
@@ -3,6 +3,10 @@ package org.cts;
 public class sample {
 public static void main(String[] args) {
 	System.out.println("sysout");
-	
+	System.out.println("ABCD");
+	System.out.println("ABCD");
+	System.out.println("ABCD");
+	System.out.println("ABCD");
+	System.out.println("ABCD");
 }
 }
