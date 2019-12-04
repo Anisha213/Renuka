f0788de83ea7a2918041a84592d0ab3362d9f6c2
Anisha213 <Anisharose213@gmail.com>
2019-12-03 18:54:20 -0800

    Code added to branch1

--- src/org/cts/sample.java
+++ src/org/cts/sample.java
@@ -3,6 +3,9 @@ package org.cts;
 public class sample {
 public static void main(String[] args) {
 	System.out.println("sysout");
-	
+	int n=10;
+	if (n%2==0) {
+		System.out.println("Odd");
+	}
 }
 }
